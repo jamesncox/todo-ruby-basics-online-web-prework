422055a24cc37d1593151a6f74cf5bb42b023827
James Cox <jamesncox@yahoo.com>
2019-02-11 18:26:52 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,13 +1,13 @@
 def division(num1, num2)
- 
+  42 / 7
 end
 
 def assign_variable(value)
-  
+  name = "Bob"
 end
 
-def argue
-  
+def argue(phrase = "I'm right and you are wrong!")
+  return "#{phrase}"
 end
 
 def greeting
